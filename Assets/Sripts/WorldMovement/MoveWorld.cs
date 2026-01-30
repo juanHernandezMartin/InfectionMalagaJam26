2a839cea7a0678b0113404943d7687b15addee68
JuanHernandezMartinTesicnor <45902436+juanHernandezMartin@users.noreply.github.com>
2026-01-30 20:14:59 +0100

    new input system plugin added

---
+++ Assets/Sripts/WorldMovement/MoveWorld.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MoveWorld : MonoBehaviour
+{
+    public float worldSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
