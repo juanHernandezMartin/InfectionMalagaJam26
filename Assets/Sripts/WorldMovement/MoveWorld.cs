4c87523f741a7840d0a2a93dd8447fbd5ffb2052
JuanHernandezMartinTesicnor <45902436+juanHernandezMartin@users.noreply.github.com>
2026-01-30 22:18:19 +0100

    human prefab

--- Assets/Sripts/WorldMovement/MoveWorld.cs
+++ Assets/Sripts/WorldMovement/MoveWorld.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class MoveWorld : MonoBehaviour
 {
     public float worldSpeed = 50f;
+    public float mouseSensitivity = 4f;
 
     // Update is called once per frame
     public void Update()
     {
         float rotationAmount = Input.GetAxis("Horizontal");
+        rotationAmount = Mathf.Clamp(rotationAmount, -1f, 1f);
         transform.Rotate(Vector3.up, rotationAmount * -worldSpeed * Time.deltaTime);
+
+        if( Input.GetMouseButton(0)) // Right mouse button held
+        {
+            float mouseRotationAmount = Input.GetAxis("Mouse X") * mouseSensitivity;
+            transform.Rotate(Vector3.up, mouseRotationAmount * -worldSpeed * Time.deltaTime);
+        }
     }
 }
