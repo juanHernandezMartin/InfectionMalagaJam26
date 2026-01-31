3e24c72de36036b4634d36a7fb56d7683d987d3d
Daviddiazgonza <daviddiazgonza@gmail.com>
2026-01-31 18:56:08 +0100

    I forgot this

--- Assets/Scripts/FactoryScript.cs
+++ Assets/Scripts/FactoryScript.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class FactoryScript : MonoBehaviour
 {
     public InventoryScript inventory;
+    private int price = 10;
 
     public void OnMouseDown()
     {
-        if (inventory.woodCount > 0)
+        if (inventory.woodCount >= price)
         {
-            inventory.woodCount--;
+            inventory.woodCount-=5;
             inventory.maskCount++;
         }
     }
