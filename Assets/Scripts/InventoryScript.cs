28bc971cce1b415dc505aa009cd1fcd34cdde245
JuanHernandezMartinTesicnor <45902436+juanHernandezMartin@users.noreply.github.com>
2026-01-31 18:15:56 +0100

    Inventory shows in the interface

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class InventoryScript : MonoBehaviour
@@ -6,4 +7,17 @@ public class InventoryScript : MonoBehaviour
     public int infectedCount = 0;
     public int healthyCount = 0;
     public int maskCount = 0;
+
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI infectedText;
+    public TextMeshProUGUI healthyText;
+    public TextMeshProUGUI maskText;
+
+    public void Update()
+    {
+        woodText.text = "Wood: " + woodCount;
+        infectedText.text = "Infected: " + infectedCount;
+        healthyText.text = "Healthy: " + healthyCount;
+        maskText.text = "Masks: " + maskCount;
+    }
 }
