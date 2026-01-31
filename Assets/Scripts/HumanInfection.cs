887e5cbc6f973c808558aecd09caec46e667290c
JuanHernandezMartinTesicnor <45902436+juanHernandezMartin@users.noreply.github.com>
2026-01-31 12:02:34 +0100

    humans die

--- Assets/Scripts/HumanInfection.cs
+++ Assets/Scripts/HumanInfection.cs
@@ -4,9 +4,27 @@ public class HumanInfection : MonoBehaviour
 {
     public Material infectedMaterial;
     public Renderer humanRenderer;
+    
+    private bool isInfected = false;
+
+    public float healthDuration = 20;
 
     public void Infect()
     {
+        isInfected = true;
         humanRenderer.material = infectedMaterial;
+    }
+
+    public void Update()
+    {
+        if (healthDuration <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (isInfected)
+        {
+            healthDuration -= Time.deltaTime;
+        }
     }
 }
