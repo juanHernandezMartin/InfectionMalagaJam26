75b2736748efe4bb00ce12a238040f1c3b7fd9b8
JuanHernandezMartinTesicnor <45902436+juanHernandezMartin@users.noreply.github.com>
2026-01-31 14:15:41 +0100

    click to mask humans

--- Assets/Scripts/HumanInfection.cs
+++ Assets/Scripts/HumanInfection.cs
@@ -4,8 +4,12 @@ public class HumanInfection : MonoBehaviour
 {
     public Material infectedMaterial;
     public Renderer humanRenderer;
+    public float maskDuration = 10f;
+    public GameObject maskObject;
     
     private bool isInfected = false;
+    private bool isMasked = false;
+    private float maskDurationTimer = 0f;
 
     public float healthDuration = 20;
 
@@ -26,6 +30,23 @@ public class HumanInfection : MonoBehaviour
         {
             healthDuration -= Time.deltaTime;
         }
+
+        if (isMasked)
+        {
+            maskDurationTimer -= Time.deltaTime;
+            if (maskDurationTimer <= 0)
+            {
+                isMasked = false;
+                maskObject.SetActive(false);
+            }
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        isMasked = true;
+        maskDurationTimer = maskDuration;
+        maskObject.SetActive(true);
     }
 
     public void OnTriggerEnter(Collider other)
